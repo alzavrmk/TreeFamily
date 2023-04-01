6beca7e55cab87e766dd25beee2f6a3315b3083e
Anastasia <3845303@gmail.com>
2023-04-01 11:04:53 +0900

    Create task1

---
+++ src/Human.java
@@ -0,0 +1,10 @@
+public class Human {
+    Human family;
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "family=" + family +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Human man = new Human();
+        System.out.println(man);
+    }
+}
